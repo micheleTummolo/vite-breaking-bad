2d37c09f5dfc71640a5fa92a8f9966a6397be9bc
micheleTummolo <tummolomichele02@gmail.com>
2023-01-14 17:25:41 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import AppContent from './components/AppContent.vue';
+
+export default {
+  components: {
+    AppContent,
+  }
+
+}
+</script>
+<template lang="">
+  <div>
+    <AppContent></AppContent>
+  </div>
+</template>
+<style lang="scss">
+@use './style/generals.scss' as *;
+</style>
